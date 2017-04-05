ea1da97e0ccdea6ae4c33bd02be55a0350a68658
MShultz <mshultz@student.neumont.edu>
2017-04-05 10:16:05 -0600

    Added Number and Decimal input capabilities.

--- app/src/main/java/shultz/mary/calculator/MainActivity.java
+++ app/src/main/java/shultz/mary/calculator/MainActivity.java
@@ -2,12 +2,48 @@ package shultz.mary.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String history = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void addToHistory(String num, String operator) {
+        history += num + operator;
+    }
+
+    private void clearHistory() {
+        history = "";
+    }
+
+    public void addDigit(View view) {
+        TextView currentInput = (TextView) findViewById(R.id.input_view);
+        int currentLength = currentInput.length();
+        Button currentButton = (Button) view;
+        String currentButtonText = currentButton.getText().toString();
+        if (isDigit(currentButtonText)) {
+            int currentDigit = Integer.parseInt(currentButtonText);
+            if ((currentDigit == 0 && currentLength >= 1) || currentDigit != 0) {
+                setText(currentDigit + "", currentInput);
+            }
+        } else {
+            setText(currentButtonText, currentInput);
+        }
+    }
+
+    private boolean isDigit(String text) {
+        return !text.equals(".");
+    }
+
+    private void setText(String text, TextView currentView) {
+        currentView.setText(currentView.getText().toString() + text);
     }
 }
